5d426a9e2f179f1895891ffbdc3a64acfaf12da9
Mauri <mauricio.stampella@25watts.com.ar>
2020-12-22 22:21:14 -0300

    Mostrando información del artista

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cancion from './components/Cancion';
+import Info from './components/Info';
 
 import axios from 'axios';
 
@@ -26,9 +27,9 @@ function App() {
       ]);
 
       guardarLetra(letra.data.lyrics);
-      guardarInfo(informacion.data.artist[0]);
+      guardarInfo(informacion.data.artists[0]);
 
-      //guardarLetra(resultado.data.lyrics)
+      // guardarLetra(resultado.data.lyrics)
     }
     consultarApiLetra();
   }, [busquedaletra]);
@@ -43,7 +44,9 @@ function App() {
       <div className="container mt-5">
         <div className="row">
           <div className="col-md-6">
-
+            <Info
+              info={info}
+            />
           </div>
           <div className="col-md-6">
             <Cancion
